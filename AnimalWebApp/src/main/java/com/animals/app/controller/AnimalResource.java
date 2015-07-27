99976c533a9afe80f497d220480696865093b15a
yra1908 <yra1908@gmail.com>
2015-07-27 22:41:48 +0300

    Adding rest methods for User and Animal

--- AnimalWebApp/src/main/java/com/animals/app/controller/AnimalResource.java
+++ AnimalWebApp/src/main/java/com/animals/app/controller/AnimalResource.java
@@ -3,11 +3,17 @@ package com.animals.app.controller;
 import com.animals.app.domain.Animal;
 import com.animals.app.repository.AnimalRepositoryImpl;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+
 import java.util.List;
 
 @Path("animals")
@@ -31,6 +37,56 @@ public class AnimalResource {
 		int idi = (int) Integer.parseInt(id);
 		
 		return anRep.getById(idi);
+		
+	}
+	
+	@DELETE
+	@Path("{animalId}") //http:localhost:8080/AnimalWebApp/webapi/animals/id
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public List<Animal> delete (@PathParam ("animalId") String animalId) {
+		
+		int idi = (int) Integer.parseInt(animalId);
+		
+		anRep.delete(idi);
+		
+		List<Animal> animalsToReturn = anRep.getAll();
+		
+		return animalsToReturn;
+		
+	}
+	
+	@POST
+	@Path("animal")//http:localhost:8080/AnimalWebApp/webapi/animals/animal
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Animal insertAnimal (Animal animal) {
+		
+		anRep.insert(animal);
+		
+		int i = anRep.getAll().size();
+		
+		return anRep.getAll().get(i-1);
+	}
+	
+	// what exactly we need from update? how we specify pet to update? if with ID than need PUT if other way - we need POST. wrong repository implementation!
+	
+	@PUT //not ready
+	@Path("animal") //http:localhost:8080/AnimalWebApp/webapi/animals/animal
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Animal update (Animal animal) {
+		
+		long id = animal.getId();
+        animal.setId(id);
+        System.out.println(id +"  -  " + animal.getId());
+		
+		anRep.update(animal);
+		
+		Animal updatedAnimal = anRep.getById(id);
+		
+		return updatedAnimal;
+		
 		
 	}
 	
--- AnimalWebApp/src/main/java/com/animals/app/controller/UserResource.java
+++ AnimalWebApp/src/main/java/com/animals/app/controller/UserResource.java
@@ -1,14 +1,19 @@
 package com.animals.app.controller;
 
-import com.animals.app.domain.User;
-import com.animals.app.repository.UserRepositoryImpl;
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+
+import com.animals.app.domain.User;
+import com.animals.app.repository.UserRepositoryImpl;
 
 @Path("users") //http:localhost:8080/AnimalsWebApp/webapi/users
 public class UserResource {
@@ -33,5 +38,56 @@ public class UserResource {
 		return userRep.getById(idi);
 		
 	}
+	
+	@DELETE
+	@Path("{userId}") //http:localhost:8080/AnimalWebApp/webapi/users/id
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public List<User> delete (@PathParam ("userId") String userId) {
+		
+		int id = (int) Integer.parseInt(userId);
+		
+		userRep.delete(id);
+		
+		List<User> usersToReturn = userRep.getAll();
+		
+		return usersToReturn;
+		
+	}
+	
+	
+	@POST
+	@Path("user")//http:localhost:8080/AnimalWebApp/webapi/users/user
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public User insertUser (User user) {
+		
+		userRep.insert(user);
+		
+		int i = userRep.getAll().size();
+		
+		return userRep.getAll().get(i-1);
+	}
+		
+	
+	@PUT //not ready
+	@Path("user") //http:localhost:8080/AnimalWebApp/webapi/users/user
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public User updateUser (User user) {
+		
+		int id = user.getId();
+        user.setId(id);
+        System.out.println(id +"  -  " + user.getId());
+		
+        userRep.update(user);
+		
+		User updatedUser = userRep.getById(id);
+		
+		return updatedUser;
+		
+	}
+	
+	
 
 }
