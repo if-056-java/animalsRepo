0031e9ffa1c0ca7ca27a87d44dbd8d4aef3531e2
yra1908 <yra1908@ukr.net>
2015-08-06 14:18:42 +0300

    Svidok angular 2

--- AnimalWebApp/src/main/webapp/services/UserProfileController.js
+++ AnimalWebApp/src/main/webapp/services/UserProfileController.js
@@ -1,12 +1,21 @@
 var animalAppControllers = angular.module('UserProfileController', []);
 
 animalApp.controller('UserProfileController', function($scope) {
-
-
-// $('#editProfile').click(function() {
-// 	console.log("ping");
-// 	$('#editUser').css("display", "block");
-// }
-
-
+	
+	$scope.IsHidden = true;
+    $scope.ShowHide = function () {
+        //If DIV is hidden it will be visible and vice versa.
+        $scope.IsHidden = $scope.IsHidden ? false : true;
+    }
 });
+
+//var app = angular.module('MyApp', [])
+//app.controller('MyController', function ($scope) {
+//    //This will hide the DIV by default.
+//    $scope.IsHidden = true;
+//    $scope.ShowHide = function () {
+//        //If DIV is hidden it will be visible and vice versa.
+//        $scope.IsHidden = $scope.IsHidden ? false : true;
+//    }
+//});
+
