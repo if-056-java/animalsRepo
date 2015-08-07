0f32f9256bf43e9305c65a7dad8e1367d1dc3948
YuriyTymchiy <tyura1@yandex.ru>
2015-08-08 00:52:25 +0300

    DB V8

--- docs/db/sql_Lost_Found_Adoption_list.sql
+++
@@ -1,3 +0,0 @@
-SELECT a.ID as ID, b.type as type, c.size as size, a.image FROM animals.animals a  
-left join animals.animaltypes b on b.id=a.typeid
-left join animals.animalsizes c on c.id=a.sizeid;
