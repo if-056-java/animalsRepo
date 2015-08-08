15ea11fcacaa8612da669f496b2ad28bdcdf19de
yra1908 <yra1908@gmail.com>
2015-08-08 18:37:05 +0300

    session emulation UserProfile

--- AnimalWebApp/src/main/webapp/services/UserProfileController.js
+++ AnimalWebApp/src/main/webapp/services/UserProfileController.js
@@ -2,24 +2,31 @@ var animalAppControllers = angular.module('UserProfileController', []);
 
 animalApp.controller('UserProfileController', function($scope, $http) {
 	
-	$scope.userInfo = null;
-	$scope.fields = null;
-	$http.get("/webapi/users/user/100")				//webapi/users/user/{id}
-    .success(function(data) {
-    	$scope.userInfo = data;
-    	$scope.fields=$scope.userInfo;
-    	console.log(data);
-    })
+	$scope.IsHidden = true;
+	$scope.showPopup = function () {$scope.IsHidden =  false;}    
+	$scope.closePopup = function () {$scope.IsHidden =  true;}
+	
+	$scope.setId=function(){
+	
+		$scope.userInfo = null;
+		$scope.fields = null;
+		var id = $scope.set.id;
+		console.log(id);
+		
+		$http.get("/webapi/users/user/" + id)				//webapi/users/user/{id}
+	    .success(function(data) {
+	    	$scope.userInfo = data;
+	    	$scope.fields=$scope.userInfo;
+	    	console.log(data);
+	    })
+	    
+	    $http.get("/webapi/users/user/"+id+"/animals")  	//webapi/users/user/{id}/animals
+	    .success(function(data) {
+	    	$scope.userAnimalInfo = data;
+	    	console.log(data);
+	    })
+	};
     
-    $http.get("/webapi/users/user/25/animals")  	//webapi/users/user/{id}/animals
-    .success(function(data) {
-    	$scope.userAnimalInfo = data;
-    	console.log(data);
-    })
-    
-    $scope.IsHidden = true;
-    $scope.showPopup = function () {$scope.IsHidden =  false;}    
-    $scope.closePopup = function () {$scope.IsHidden =  true;}
        
          
     $scope.submitUpdateForm=function(){    	
