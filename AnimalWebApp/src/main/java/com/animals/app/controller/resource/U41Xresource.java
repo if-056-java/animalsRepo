2c1e5f4fc94cfbd0c0fc825e982ad0e75532d6bf
yra1908 <yra1908@gmail.com>
2015-08-08 12:30:44 +0300

    Angular - kurvuy sun :)

--- AnimalWebApp/src/main/java/com/animals/app/controller/resource/U41Xresource.java
+++ AnimalWebApp/src/main/java/com/animals/app/controller/resource/U41Xresource.java
@@ -1,12 +1,12 @@
 package com.animals.app.controller.resource;
 
-import java.util.List;
-
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -24,7 +24,7 @@ public class U41Xresource {
 	
 	@GET //http:localhost:8080/webapi/users/id
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("{userId}") 
+	@Path("user/{userId}") 
 	public Response getUserById(@PathParam ("userId") String id) {
 				
 		int parseId = 0;
@@ -41,25 +41,47 @@ public class U41Xresource {
 		
 		if (user == null) return NOT_FOUND;
 		
-		return Response.ok().entity(user).build();		
-	
+		return Response.ok().entity(user).build();	
 		
 	}
 	
-	@GET //http:localhost:8080/webapi/users
-	@Produces(MediaType.APPLICATION_JSON)	
-	public Response getAllUsers() {
+	@POST
+	@Path("user")//http:localhost:8080/webapi/users/user
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response insertUser (User user) {
 		
-		List<User> users=userRep.getAll();
+		if (user==null) return BAD_REQUEST;
 		
-		GenericEntity<List<User>> genericUsers =new GenericEntity<List<User>>(users) {};
-
-		//if(genericUsers == null) return NOT_FOUND;  //We really need it???
-
-		return Response.ok().entity(genericUsers).build(); 
+		userRep.insert(user);
+		
+		return Response.ok().entity(user).build();
 		
 	}
 	
-	
+	@PUT 
+	@Path("user") //http:localhost:8080/webapi/users/user
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateUser (User user) {
+		
+		if (user==null) return BAD_REQUEST;
+		
+		int id;		
+		try {
+			id = user.getId();
+		} catch (Exception e) {
+			return BAD_REQUEST;
+		}		
+				
+		if (userRep.getById(id) == null) return NOT_FOUND;     
+		
+        userRep.update(user);
+		
+		User updatedUser = userRep.getById(id);
+		
+		return Response.ok().entity(updatedUser).build();
+		
+	}
 
 }
--- AnimalWebApp/src/main/webapp/services/UserProfileController.js
+++ AnimalWebApp/src/main/webapp/services/UserProfileController.js
@@ -2,21 +2,30 @@ var animalAppControllers = angular.module('UserProfileController', []);
 
 animalApp.controller('UserProfileController', function($scope, $http) {
 	
-	//$scope.userInfo = 
-	$http.get("/webapi/users/25")
+	$scope.userInfo = null;
+	$scope.fields = null;
+	$http.get("/webapi/users/user/100")
     .success(function(data) {
     	$scope.userInfo = data;
+    	$scope.fields=$scope.userInfo;
     	console.log(data);
     })
+    
+    $scope.IsHidden = true;
+    $scope.showPopup = function () {$scope.IsHidden =  false;}    
+    $scope.closePopup = function () {$scope.IsHidden =  true;}
+       
+         
+    $scope.submitUpdateForm=function(){    	
+    	
+		var data=$scope.fields; 
+        console.log(data);
+		
+        $http.put("/webapi/users/user", data)
+        .success(function(data){
+        	console.log(data);      
+        });  
 	
-	$scope.IsHidden = true;
-    $scope.showPopup = function () {
-        //show div.
-        $scope.IsHidden =  false;
-    }    
-    $scope.closePopup = function () {
-    	 //hide div.
-        $scope.IsHidden =  true;
-    }
+	};  
 	
 });
