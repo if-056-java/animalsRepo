3bb9f0b4eb5abe78679e5ebe4315bc4f23f42d84
Rostislav.Viner <vkilgara@gmail.com>
2015-08-14 11:54:28 +0300

    merge admin_animal_detailed and admin_animal_editor

--- AnimalWebApp/src/main/webapp/controllers/AdminAnimalsDetailedController.js
+++ AnimalWebApp/src/main/webapp/controllers/AdminAnimalsDetailedController.js
@@ -1,8 +1,18 @@
-angular.module('AdminAnimalsDetailed', ['AdminAnimalsModule'])
+angular.module('AdminAnimalsDetailed', ['AdminAnimalsModule', 'angularFileUpload', 'nya.bootstrap.select', 'ui.bootstrap'])
     .controller('AdminAnimalsDetailedController', ['$scope', 'AdminAnimalsService', '$routeParams', '$window',
         function($scope, AdminAnimalsService, $routeParams, $window) {
-        
+
             var animalId = $routeParams.animalId;   //route parameter - animal id
+            var AADC = this;
+            $scope.editor = false;
+            $scope.animalImage = undefined;
+
+            /**
+             * back to the previous page.
+             */
+            $scope.goBack = function() {
+                $window.history.back();
+            }
 
             /**
              * @param animalId id of animal used for lookup.
@@ -13,7 +23,7 @@ angular.module('AdminAnimalsDetailed', ['AdminAnimalsModule'])
                     .then(function(data) {
                         $scope.animal = data;
                         if ($scope.animal.image != undefined) {
-                            $scope.animal.image = $scope.animal.image + "?timestamp=" + new Date().getTime();
+                            $scope.animalImage = $scope.animal.image + "?timestamp=" + new Date().getTime();
                         }
                     },
                     function(data) {
@@ -35,4 +45,166 @@ angular.module('AdminAnimalsDetailed', ['AdminAnimalsModule'])
                         $window.alert("Animal delete failed.");
                     });
             }
+
+            $scope.showEditor = function() {
+                $scope.editor = true;
+            }
+        }])
+    .controller('AdminAnimalsEditorController', ['$scope', 'AdminAnimalsService', '$filter',
+        function($scope, AdminAnimalsService, $filter) {
+            $scope.hideEditor = function() {
+                $scope.$parent.editor = false;
+                $scope.$parent.animalImage = $scope.$parent.animalImage + "?timestamp=" + new Date().getTime();
+            }
+
+            /**
+             * @return list of animal types.
+             */
+            $scope.$parent.$watch('editor', function(newValue, oldValue) {
+                if (newValue === false || $scope.animalTypes != undefined) {
+                    return;
+                }
+                AdminAnimalsService.getAnimalTypes()
+                    .then(function (data) {
+                        $scope.animalTypes = data;
+                    },
+                    function (data) {
+                        console.log('Animal retrieval failed.')
+                    });
+            });
+
+            /**
+             * @return list of animal breeds according to animal type.
+             */
+            $scope.$parent.$watch('animal.type', function() {
+                if ($scope.$parent.animal == undefined) {
+                    return;
+                }
+                $scope.animalBreeds = undefined;
+                $scope.$parent.animal.breed = undefined;
+                $scope.filterAnimalBreedFlag = true;
+                AdminAnimalsService.getAnimalBreeds($scope.$parent.animal.type.id)
+                    .then(function(data) {
+                        $scope.animalBreeds = data;
+                    },
+                    function(data) {
+                        console.log('Animal breeds retrieval failed.')
+                    })
+                    .finally(function() {
+                        $scope.filterAnimalBreedFlag = false;
+                    });
+
+            });
+
+            /**
+             * update animal.
+             */
+            $scope.setAnimal = function() {
+                $scope.$parent.animal.dateOfBirth = $filter('date')($scope.$parent.animal.dateOfBirth, 'yyyy-MM-dd');
+                $scope.$parent.animal.dateOfSterilization = $filter('date')($scope.$parent.animal.dateOfSterilization, 'yyyy-MM-dd');
+                $scope.$parent.animal.dateOfRegister = $filter('date')($scope.$parent.animal.dateOfRegister, 'yyyy-MM-dd');
+                if (typeof $scope.$parent.animal.breed != "undefined") {
+                    if (typeof $scope.$parent.animal.breed.id == "undefined") {
+                        $scope.$parent.animal.breed = {breedUa: $scope.$parent.animal.breed};
+                    }
+                }
+
+                AdminAnimalsService.updateAnimal($scope.$parent.animal)
+                    .then(function(data) {
+                        $scope.$parent.editor = false;
+                        $scope.hideEditor();
+                    },
+                    function(data) {
+                        console.log('Animal update failed.')
+                    });
+            }
+        }])
+    .controller('DPController', ['$scope', function($scope) {
+
+        $scope.clear = function () {
+            $scope.dt = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+        };
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        var afterTomorrow = new Date();
+        afterTomorrow.setDate(tomorrow.getDate() + 2);
+        $scope.events = [
+            {date: tomorrow, status: 'full'},
+            {date: afterTomorrow, status: 'partially'}
+        ];
+
+        $scope.getDayClass = function(date, mode) {
+            if (mode === 'day') {
+                var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+                for (var i=0;i<$scope.events.length;i++){
+                    var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+                    if (dayToCheck === currentDay) {
+                        return $scope.events[i].status;
+                    }
+                }
+            }
+
+            return '';
+        };
+
+    }])
+    .controller('AdminAnimalsEditorSetImageController', ['$scope', 'FileUploader', function($scope, FileUploader) {
+        var uploader = $scope.uploader = new FileUploader();
+
+        uploader.autoUpload = true;         //Automatically upload files after adding them to the queue
+        uploader.removeAfterUpload = true;  //Remove files from the queue after uploading
+
+        /**
+         * add filters.
+         */
+        uploader.filters.push({
+            name: 'imageFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        });
+
+        /**
+         * Fires before uploading an item.
+         * @param item to be uploaded to the server
+         */
+        uploader.onBeforeUploadItem = function(item) {
+            item.url = "/webapi/admin/animals/editor/upload/" + $scope.animal.id;
+            item.formData = {animalId: $scope.animal.id};
+        }
+
+        /**
+         * On file upload complete.
+         * @param fileItem uploaded to the server
+         * @param response of server
+         * @param status response status
+         * @param headers response headers
+         */
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            if (status == 200) {
+                $scope.$parent.animal.image = response.filePath;
+                $scope.$parent.animalImage = response.filePath + "?timestamp=" + new Date().getTime();
+            }
+        };
     }]);
--- AnimalWebApp/src/main/webapp/modules/animalApp.js
+++ AnimalWebApp/src/main/webapp/modules/animalApp.js
@@ -13,82 +13,76 @@ var animalApp = angular.module('animalApp', [
     'StarterPageController',
     'AdminAnimals',
     'AdminAnimalsDetailed',
-    'AdminAnimalsEditor',
     'RegistrationController',
     'UserProfileController',
     'AnimalShortInfoController'
 ]);
 
 animalApp .config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/ua', {
-        templateUrl: 'views/main_view.html',
-        controller: 'StarterPageController'
-      }).
-	  when('/ua/animal/adoption', {
-        templateUrl: 'views/adoption.html',
-        controller: 'AdoptionController'
-      }).
-	  when('/ua/animal/find', {
-        templateUrl: 'views/find_lost.html',
-        controller: 'FindController'
-      }).
-	  when('/ua/animal/lost', {
-        templateUrl: 'views/find_lost.html',
-        controller: 'LostController'
-      }).
-	  when('/ua/animal/homeless', {
-        templateUrl: 'views/reg_homeless.html',
-        controller: 'HomelessController'
-      }).
-	  when('/ua/animal/detail', {
-        templateUrl: 'views/admin_animals_detailed.html',
-        controller: 'AnimalsDetailController'
-      }).
-	  when('/ua/contacts', {
-        templateUrl: 'views/contacts.html',
-        controller: 'ContactsController'
-      }).
-      when('/ua/user/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginController'
-      }).
-        when('/ua/user/home/animals', {
-          templateUrl: 'views/admin_animals.html',
-          controller: 'AdminAnimalsController'
-        }).
-        when('/ua/user/home/animals/:animalId', {
-          templateUrl: 'views/admin_animals_detailed.html',
-          controller: 'AdminAnimalsDetailedController'
-        }).
-        when('/ua/user/home/animals/editor/:animalId', {
-            templateUrl: 'views/admin_animals_editor.html',
-            controller: 'AdminAnimalsEditorController'
-        }).
-      when('/ua/user/registration', {
-        templateUrl: 'views/registration.html',
-        controller: 'RegistrationController'
-      }).
-      when('/ua/user/profile', {
-        templateUrl: 'views/user_profile.html',
-        controller: 'UserProfileController'
-      }).
-	  when('/ua/animal/:service/:animalId', {
-        templateUrl: 'views/animal_short_info.html',
-        controller: 'AnimalShortInfoController'
-      }).
-      otherwise({
-        redirectTo: '/ua'
-      });
-  }]);
+    function($routeProvider) {
+        $routeProvider.
+            when('/ua', {
+                templateUrl: 'views/main_view.html',
+                controller: 'StarterPageController'
+            }).
+            when('/ua/animal/adoption', {
+                templateUrl: 'views/adoption.html',
+                controller: 'AdoptionController'
+            }).
+            when('/ua/animal/find', {
+                templateUrl: 'views/find_lost.html',
+                controller: 'FindController'
+            }).
+            when('/ua/animal/lost', {
+                templateUrl: 'views/find_lost.html',
+                controller: 'LostController'
+            }).
+            when('/ua/animal/homeless', {
+                templateUrl: 'views/reg_homeless.html',
+                controller: 'HomelessController'
+            }).
+            when('/ua/animal/detail', {
+                templateUrl: 'views/admin_animals_detailed.html',
+                controller: 'AnimalsDetailController'
+            }).
+            when('/ua/contacts', {
+                templateUrl: 'views/contacts.html',
+                controller: 'ContactsController'
+            }).
+            when('/ua/user/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginController'
+            }).
+            when('/ua/user/home/animals', {
+                templateUrl: 'views/admin_animals.html',
+                controller: 'AdminAnimalsController'
+            }).
+            when('/ua/user/home/animals/:animalId', {
+                templateUrl: 'views/admin_animals_detailed.html',
+                controller: 'AdminAnimalsDetailedController'
+            }).
+            when('/ua/user/registration', {
+                templateUrl: 'views/registration.html',
+                controller: 'RegistrationController'
+            }).
+            when('/ua/user/profile', {
+                templateUrl: 'views/user_profile.html',
+                controller: 'UserProfileController'
+            }).
+            when('/ua/animal/:service/:animalId', {
+                templateUrl: 'views/animal_short_info.html',
+                controller: 'AnimalShortInfoController'
+            }).
+            otherwise({
+                redirectTo: '/ua'
+            });
+    }]);
 
 //Constants
 animalApp.constant('RESOURCES', {
     RESOURCE: 'http://127.0.0.1:8080/',
-        ANIMALS_FOR_ADOPTING_PAGINATOR: 'webapi/animals/adoption/pagenator',
-        ANIMALS_FOR_ADOPTING: 'webapi/animals/adoption',
-        ANIMAL_TYPES: 'webapi/animals/animal_types',
-        ANIMAL_BREEDS: 'webapi/animals/animal_breeds/'
+    ANIMALS_FOR_ADOPTING_PAGINATOR: 'webapi/animals/adoption/pagenator',
+    ANIMALS_FOR_ADOPTING: 'webapi/animals/adoption',
+    ANIMAL_TYPES: 'webapi/animals/animal_types',
+    ANIMAL_BREEDS: 'webapi/animals/animal_breeds/'
 });
-
