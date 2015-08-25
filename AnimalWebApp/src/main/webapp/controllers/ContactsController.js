d7e869694562fe12f932edb06b8dfe9b800669bf
YuriyTymchiy <mama1@i.ua>
2015-08-25 15:58:23 +0300

    Cosmetics

--- AnimalWebApp/src/main/webapp/controllers/ContactsController.js
+++ AnimalWebApp/src/main/webapp/controllers/ContactsController.js
@@ -1,20 +1,21 @@
-var animalAppControllers = angular.module('ContactsController', ['vcRecaptcha']);
+var animalAppControllers = angular.module('ContactsController', ['vcRecaptcha','ContactsValues']);
 
-animalApp.controller('ContactsController', function ($http, $scope, vcRecaptchaService) {
+animalApp.controller('ContactsController', function ($http, $scope, vcRecaptchaService, ContactsValues) {
 
-    var myLatlng = new google.maps.LatLng(49.863400, 24.044500);
-    var mapOptions = {
-            zoom: 16,
+    var myLatlng = new google.maps.LatLng(ContactsValues.googlemap.latitude, ContactsValues.googlemap.longitude),
+        mapOptions = {
+            zoom: ContactsValues.googlemap.zoom,
             center: myLatlng
-        };
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    var marker = new google.maps.Marker({
+        },
+        map = new google.maps.Map(document.getElementById('map'), mapOptions),
+        marker = new google.maps.Marker({
             position: myLatlng,
             draggable: true,
             map: map,
             animation: google.maps.Animation.DROP,
-            title: 'Наше розташування!'
+            title: ContactsValues.googlemap.markerhint
         });
+
     google.maps.event.addListener(marker, 'click', toggleBounce);
 
     function toggleBounce() {
@@ -25,33 +26,12 @@ animalApp.controller('ContactsController', function ($http, $scope, vcRecaptchaS
             marker.setAnimation(google.maps.Animation.BOUNCE);
         }
     }
-        var opts = {
-            lines: 10 // The number of lines to draw
-            , length: 10 // The length of each line
-            , width: 8 // The line thickness
-            , radius: 18 // The radius of the inner circle
-            , scale: 1 // Scales overall size of the spinner
-            , corners: 1 // Corner roundness (0..1)
-            , color: 'green' // #rgb or #rrggbb or array of colors
-            , opacity: 0.35 // Opacity of the lines
-            , rotate: 0 // The rotation offset
-            , direction: 1 // 1: clockwise, -1: counterclockwise
-            , speed: 2 // Rounds per second
-            , trail: 53 // Afterglow percentage
-            , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-            , zIndex: 2e9 // The z-index (defaults to 2000000000)
-            , className: 'spinner' // The CSS class to assign to the spinner
-            , top: '50%' // Top position relative to parent
-            , left: '80%' // Left position relative to parent
-            , shadow: false // Whether to render a shadow
-            , hwaccel: false // Whether to use hardware acceleration
-            , position: 'absolute' // Element positioning
-        };
+
 
         //initialize loading spinner
         var target = document.getElementById('loading-block')
-        new Spinner(opts).spin(target);
-        
+        new Spinner(ContactsValues.spinneropts).spin(target);
+
         $scope.spinnerloading = 0;
         $scope.startupmessage = 1;
         $scope.okmessage = 0;
@@ -59,9 +39,8 @@ animalApp.controller('ContactsController', function ($http, $scope, vcRecaptchaS
 
     $scope.response = null;
     $scope.widgetId = null;
-    $scope.model = {
-        key: '6LdeAQsTAAAAAFYDoWkW8_zNKVu1tu6D-RlLfgnR'
-    };
+    $scope.model = ContactsValues.recaptcha;
+
     $scope.setResponse = function (response) {
         $scope.response = response;
     };
@@ -79,7 +58,7 @@ animalApp.controller('ContactsController', function ($http, $scope, vcRecaptchaS
            $scope.errormessage = 0;
 
         } else {
-            $scope.spinnerloading = 1;
+           $scope.spinnerloading = 1;
            $scope.startupmessage = 0;
            $scope.okmessage = 0;
            $scope.errormessage = 0;
