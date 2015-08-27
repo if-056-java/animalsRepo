9e24a2c8e482fa94ed18ab5ec4d5abb4e4052c15
Vova Yevdoshenko <vovayevdoshenko@gmail.com>
2015-08-27 15:45:06 +0300

    little changes to short info

--- AnimalWebApp/src/main/webapp/controllers/AnimalShortInfoController.js
+++ AnimalWebApp/src/main/webapp/controllers/AnimalShortInfoController.js
@@ -10,22 +10,21 @@ angular.module('AnimalShortInfoController', ['AnimalShortInfoService'])
 			
 			if(service === "found"){
 			    $scope.detail=false;
-				$scope.textButton="";
+				$scope.textButton="Contact";
 				$scope.linkButton="#";
-				$scope.linkBack="#/ua/animal/adoption"
-			}
+				$scope.linkBack="#/ua/animal/found"
+			};
 			if(service === "adoption"){
 			    $scope.detail=true;
 				$scope.textButton="Adopt";
-				$scope.linkButton="#"
-				$scope.linkBack="#/ua/animal/adoption"
-				
-			}
+				$scope.linkButton="#";
+				$scope.linkBack="#/ua/animal/adoption";
+			};
 			if(service === "lost") {
 			    $scope.detail=true;
-				$scope.textButton="";
+				$scope.textButton="Contact";
 				$scope.linkButton="#";
-				$scope.linkBack="#/ua/animal/adoption"
+				$scope.linkBack="#/ua/animal/lost";
 			}
 
             this.getAnimal = function(service, animalId) {
@@ -34,7 +33,7 @@ angular.module('AnimalShortInfoController', ['AnimalShortInfoService'])
                         $scope.animal = data;
                     },
                     function(data) {
-                        console.log('Animal retrieval failed.')
+                        console.log('Animal retrieval failed.');
                     });
             };
             this.getAnimal(service, animalId);
--- AnimalWebApp/src/main/webapp/services/AnimalShortInfoService.js
+++ AnimalWebApp/src/main/webapp/services/AnimalShortInfoService.js
@@ -13,6 +13,6 @@ angular.module('AnimalShortInfoService', [])
                 });
 
             return def.promise;
-        }
+        };
 		
     }]);
