8f782efd71c2913ff80fe335983b603cc39a78e5
yra1908 <yra1908@ukr.net>
2015-09-15 09:33:10 +0300

    user reg validation

--- AnimalWebApp/src/main/webapp/controllers/RegistrationController.js
+++ AnimalWebApp/src/main/webapp/controllers/RegistrationController.js
@@ -15,30 +15,27 @@ animalApp.controller('RegistrationController', ['$scope', '$location', '$route',
 			
 			$scope.fields.registrationDate = currentDate;
 			
-			if ($scope.fields.password!=$scope.fields.password_confirm){
-				$scope.errorConfirmMessage=true;				
-			} else {				
-				$scope.fields.password=hashPassword($scope.fields.password); 
-				AuthenticationService.registerUser($scope.fields).then(
-					function(result){					
-						if(result.userId==0){
-				        	$scope.errorRegistrationMessage1="Помилка реєстрації. Даний логін вже використовується!";
-				        	console.log("Registration error. SocialLogin is already exist");
+							
+			$scope.fields.password=hashPassword($scope.password); 
+			AuthenticationService.registerUser($scope.fields).then(
+				function(result){					
+					if(result.userId==0){
+				        $scope.errorRegistrationMessage1="Помилка реєстрації. Даний логін вже використовується!";
+				        console.log("Registration error. SocialLogin is already exist");
 				        		
-				        } else if (result.userId==1){	
-				        	$location.path("/ua/user/confirmRegistration");	
-						    $route.reload();						        
-				        } else {
-				        	console.log("error");
-				        	$scope.errorRegistrationMessage3="Помилка реєстрації";
-				       	}			
-					},
-					function(error){
-						console.log(error);					
-						$scope.errorRegistrationMessage2="Помилка реєстрації! Спробуйте ще раз";
-					}
-				);
-			}     
+				    } else if (result.userId==1){	
+				        $location.path("/ua/user/confirmRegistration");	
+						$route.reload();						        
+				    } else {
+				        console.log("error");
+				        $scope.errorRegistrationMessage3="Помилка реєстрації";
+				    }			
+				},
+				function(error){
+					console.log(error);					
+					$scope.errorRegistrationMessage2="Помилка реєстрації! Спробуйте ще раз";
+				}
+			);    
 	        
 		}; 
 		
