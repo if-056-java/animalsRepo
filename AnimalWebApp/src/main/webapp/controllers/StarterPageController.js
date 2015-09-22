bbbaf9594b6274f3f6ce87b1a0c1e447208ee3d0
YuriyTymchiy <tyura1@yandex.ru>
2015-09-23 02:10:21 +0300

    #105

--- AnimalWebApp/src/main/webapp/controllers/StarterPageController.js
+++ AnimalWebApp/src/main/webapp/controllers/StarterPageController.js
@@ -2,5 +2,4 @@ var animalAppControllers = angular.module('StarterPageController', []);
 
 animalApp.controller('StarterPageController', function($scope, $window) {
     $scope.currentLanguage = $window.localStorage.getItem('NG_TRANSLATE_LANG_KEY');
-    console.log($scope.currentLanguage);
 });
