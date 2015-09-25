70628a9f72da5887f5caad71529c4bd4c1a64a15
yra1908 <yra1908@ukr.net>
2015-09-25 10:09:15 +0300

    User Moderation Ready

--- AnimalWebApp/src/main/webapp/controllers/UserEditorAdminController.js
+++ AnimalWebApp/src/main/webapp/controllers/UserEditorAdminController.js
@@ -7,16 +7,20 @@ animalApp.controller('UserEditorAdminController', ['$scope', 'UserModerationServ
     var targetContent = document.getElementById('loading-block');
     new Spinner(opts).spin(targetContent);
     //This variable decides when spinner loading for contentis closed.
-    $scope.contentLoading = 1;
-    
-    $scope.currentLanguage = localStorage.getItem("NG_TRANSLATE_LANG_KEY");
+    $scope.contentLoading = 1;    
 	
 	var id = $routeParams.userId;	
 	
 	UserModerationService.getUser(id).then(
 			function(result){
 				$scope.user=result;				
-				$scope.contentLoading--;				
+				$scope.contentLoading--;
+				if($scope.user.active){ 
+						$scope.active="true";
+					} else {
+						$scope.active="false";
+					}
+				;
 			},
 			function(error){					
 				console.log(error)
@@ -25,14 +29,25 @@ animalApp.controller('UserEditorAdminController', ['$scope', 'UserModerationServ
 		);
 	
 	$scope.updateUser = function() {
+		
+		if($scope.user.userRole[0].role=="moderator")$scope.user.userRole[0].id=1
+		if($scope.user.userRole[0].role=="guest")$scope.user.userRole[0].id=3;
+		if($scope.user.userRole[0].role=="doctor")$scope.user.userRole[0].id=4;
+		
+		if($scope.user.userType.type=="власник")$scope.user.userType.id=1;
+		if($scope.user.userType.type=="ветеринар")$scope.user.userType.id=2;
+		
+		if($scope.active=="true")$scope.user.active=true;
+		if($scope.active=="false")$scope.user.active=false;
+	
    	    	 
-   	UserModerationService.updateUser($scope.user, id)
-           .then(function(data) {
-               $window.location.href = "#/ua/user/admin/users/" + id;
-           },
-           function(error) {
-               $window.alert("Animal update failed.");
-           });
+	   	UserModerationService.updateUser($scope.user, id)
+	           .then(function(data) {
+	               $window.location.href = "#/ua/user/admin/users/" + id;
+	           },
+	           function(error) {
+	               $window.alert("Animal update failed.");
+	           });
    }
 	
 	
