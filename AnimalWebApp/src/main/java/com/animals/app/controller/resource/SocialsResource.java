1cab481ea673ed3b10149c2818ae0a669c967ba0
YuriyTymchiy <mama1@i.ua>
2015-09-29 12:31:53 +0300

    Socials refactoring

--- AnimalWebApp/src/main/java/com/animals/app/controller/resource/SocialsResource.java
+++ AnimalWebApp/src/main/java/com/animals/app/controller/resource/SocialsResource.java
@@ -4,24 +4,27 @@ import com.animals.app.domain.Animal;
 import com.animals.app.repository.AnimalRepository;
 import com.animals.app.repository.Impl.AnimalRepositoryImpl;
 import com.animals.app.service.Facebook;
+import com.animals.app.service.FetchConfigProperties;
 import com.animals.app.service.Twitt;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 @Path("socials")
+
+@RolesAllowed("moderator")
+
 public class SocialsResource {
     private static Logger LOG = LogManager.getLogger(SocialsResource.class);
+
+    private final Properties socialsConfig = new Properties();
 
     private final Response BAD_REQUEST = Response.status(Response.Status.BAD_REQUEST).build();
 
@@ -41,10 +44,13 @@ public class SocialsResource {
         Twitt twitt;
         String consumerKey, consumerSecret, accessToken, accessTokenSecret, messageBody ;
 
-        System.out.println("animalId -" + animalId);
         if (animalId == 0) {
+            LOG.warn("Wrong animal ID!");
             return BAD_REQUEST;
         }
+
+        getProperties();
+
         consumerKey = socialsConfig.getProperty("twitter.consumerKey");
         consumerSecret = socialsConfig.getProperty("twitter.consumerSecret");
         accessToken = socialsConfig.getProperty("twitter.accessToken");
@@ -56,6 +62,7 @@ public class SocialsResource {
         Animal animal = animalRepository.getById(animalId);
 
         if (animal == null) {
+            LOG.warn("Wrong animal ID!");
             return NOT_FOUND;
         }
         if (animal.getDateOfTwitter() == null) {
@@ -72,14 +79,17 @@ public class SocialsResource {
 
             if (twitt.sendTwitt(consumerKey, consumerSecret, accessToken, accessTokenSecret)) {
                 animal.setDateOfTwitter(getCurrentDate());
-                System.out.println("Date: - " + animal.getDateOfTwitter().toString());
                 animalRepository.twitterUpdate(animal);
                 return OK;
-            } else
+            } else {
+                LOG.error("Twitter send error");
                 return BAD_REQUEST;
+            }
         }
-        else
+        else {
+            LOG.error("Error in DB - see dateOfTwitter column!");
             return BAD_REQUEST;
+        }
     }
 
     @POST //http:localhost:8080/webapi/socials/facebook/animalId
@@ -90,8 +100,8 @@ public class SocialsResource {
         Facebook facebook;
         String accessToken, wallId ;
 
-        System.out.println("animalId -" + animalId);
         if (animalId == 0) {
+            LOG.warn("Wrong animal ID!");
             return BAD_REQUEST;
         }
 
@@ -100,33 +110,32 @@ public class SocialsResource {
         Animal animal = animalRepository.getById(animalId);
 
         if (animal == null) {
+            LOG.warn("Wrong animal ID!");
             return NOT_FOUND;
         }
 
         if (animal.getDateOfFacebook() == null) {
 
+            getProperties();
+
             accessToken = socialsConfig.getProperty("facebook.accessToken");
             wallId = socialsConfig.getProperty("facebook.wallId");
 
-            //attach any media, if you want to
-//            if (!animal.getImage().equals(null)) {
-  //              String restPath = httpServlet.getServletContext().getRealPath("/"); //path to rest root folder
-    //            twitt.setMedia(restPath + animal.getImage());
-      //      }
+            facebook = new Facebook();
 
-                facebook = new Facebook();
-
-
-            if (facebook.sendFacebook(accessToken, wallId, getCurrentDate().toString()+animalId)) {
+            if (facebook.sendFacebook(accessToken, wallId, "Нова тварина! Деталі: - http://tym.dp.ua/#/ua/animal/adoption/" + animalId)) {
                 animal.setDateOfFacebook(getCurrentDate());
-                System.out.println("Date: - " + animal.getDateOfFacebook().toString());
                 animalRepository.facebookUpdate(animal);
                 return OK;
-            } else
+            } else {
+                LOG.warn("Facebook error!");
                 return BAD_REQUEST;
+            }
         }
-        else
+        else {
+            LOG.error("Error in DB - see dateOfFacebook column!");
             return BAD_REQUEST;
+        }
 
     }
 
@@ -145,25 +154,8 @@ public class SocialsResource {
         return Response.ok().entity(entity).build();
     }
 
-    private static Properties socialsConfig = new Properties();
-    {
-        fetchConfig();
+    public void getProperties() {
+        new FetchConfigProperties().fetchConfig(socialsConfig);
     }
 
-    /**
-     * Open a specific text file containing reCAPTCHA secret key
-     * and verification URL.
-     */
-    private void fetchConfig() {
-        //This file contains the reCAPTCHA config properties mentioned above.
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("project.properties").getFile());
-
-        try (InputStream input = new FileInputStream(file)) {
-            socialsConfig.load(input);
-        }
-        catch (IOException ex){
-            System.err.println("Cannot open and load mail server properties file. Put it on...");
-        }
-    }
 }
---
+++ AnimalWebApp/src/main/java/com/animals/app/service/FetchConfigProperties.java
@@ -0,0 +1,30 @@
+package com.animals.app.service;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by aquarius on 9/28/2015.
+ */
+public class FetchConfigProperties {
+    private static Logger LOG = LogManager.getLogger(FetchConfigProperties.class);
+
+    public void fetchConfig(Properties prop) {
+        //This file contains the reCAPTCHA config properties mentioned above.
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("project.properties").getFile());
+
+        try (InputStream input = new FileInputStream(file)) {
+            prop.load(input);
+        }
+        catch (IOException e){
+            LOG.error(e);
+        }
+    }
+}
